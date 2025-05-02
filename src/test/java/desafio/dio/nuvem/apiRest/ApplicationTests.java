b0dbd8a30c26dbd8b61a060399090ca90f830810
Gui Ferreira <guifl_pereira@hotmail.com>
2025-05-02 11:50:33 -0300

    Primeiro commit

---
+++ src/test/java/desafio/dio/nuvem/apiRest/ApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.dio.nuvem.apiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
